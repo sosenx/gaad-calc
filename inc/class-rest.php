52ce286fe42e43f9fd0d7a74ab8b822d5a54a9fe
sosenx <barteksosnowski711@gmail.com>
2018-03-02 21:37:04 +0100

    cors

--- inc/class-rest.php
+++ inc/class-rest.php
@@ -70,11 +70,7 @@ class rest{
 	}
 
 	public static function rest_calculate_callback( $data = NULL ){
-<<<<<<< HEAD
 		\gcalc\rest::cors();
-=======
-		gcalc\rest::cors();
->>>>>>> e13d0875147b9f30ee45bb9ed3ecc1feac044ac8
 		$h = \gcalc\rest::getHeaders( "/^pa_.*|^product_.*|^group_.*|apikey|apisecret|Authorization/", true ); 
 		$product_id = \gcalc\rest::getHeaders( "/product_id/" );	
 		$product_id = count( $product_id ) == 0 ? NULL : (int) $product_id[ "product_id" ];
@@ -103,42 +99,12 @@ class rest{
 	}
 
 
-	/**
-	 *  An example CORS-compliant method.  It will allow any GET, POST, or OPTIONS requests from any
-	 *  origin.
-	 *
-	 *  In a production environment, you probably want to be more restrictive, but this gives you
-	 *  the general idea of what is involved.  For the nitty-gritty low-down, read:
-	 *
-	 *  - https://developer.mozilla.org/en/HTTP_access_control
-	 *  - http://www.w3.org/TR/cors/
-	 *
-	 */
-	public static function cors() {
 
-	    // Allow from any origin
-	    if (isset($_SERVER['HTTP_ORIGIN'])) {
-	        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-	        // you want to allow, and if so:
-	        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-	        header('Access-Control-Allow-Credentials: true');
-	        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-	    }
 
-	    // Access-Control headers are received during OPTIONS requests
-	    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-	        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-	            // may also be using PUT, PATCH, HEAD etc
-	            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
 
-	        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-	            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-	        
-	    }
 
-	    
-	}
+
 
 
 	/**
